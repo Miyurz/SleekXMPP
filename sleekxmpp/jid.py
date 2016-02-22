9ea37a8555b03c9313f3502e8e09388f1e640e9b
Miyurz <mayur.nagekar@gmail.com>
2016-02-22 15:21:10 +0530

    Is _ really an illegal character ?
    While working on a chats client, I observed that my Jabber ID had _ as hence xmpp package kept complaining about it. I did a bit of research to find if its really unacceptable. 
    https://developer.pidgin.im/ticket/13305
    
    Views ?

--- sleekxmpp/jid.py
+++ sleekxmpp/jid.py
@@ -28,7 +28,7 @@ from sleekxmpp.thirdparty import OrderedDict
 ILLEGAL_CHARS = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r' + \
                 '\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19' + \
                 '\x1a\x1b\x1c\x1d\x1e\x1f' + \
-                ' !"#$%&\'()*+,./:;<=>?@[\\]^_`{|}~\x7f'
+                ' !"#$%&\'()*+,./:;<=>?@[\\]^`{|}~\x7f'
 
 #: The basic regex pattern that a JID must match in order to determine
 #: the local, domain, and resource parts. This regex does NOT do any
